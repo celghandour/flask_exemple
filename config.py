1d223b966627647ceda4362f5b3fb4f2cb299884
Chafik <elghandour.chafik@gmail.com>
2020-11-09 13:55:14 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+
+# Database initialization
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+print(os.getcwd())
