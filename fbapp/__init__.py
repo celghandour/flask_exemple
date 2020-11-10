5158ce416dfcfa699d5a085ed5850963c71eb3db
Chafik <elghandour.chafik@gmail.com>
2020-11-10 17:29:23 +0100

    add template index.html

--- fbapp/__init__.py
+++ fbapp/__init__.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask
-from .views import app
-from . import models
+from fbapp.views import app
+from fbapp import models
 
 # Connect sqlalchemy to app
 models.db.init_app(app)
 
 
-@app.cli.command()
+@app.cli.command("init_db")
 def init_db():
     models.init_db()
--- fbapp/models.py
+++ fbapp/models.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+import logging as lg
+
+import enum
 
 from fbapp.views import app
 
@@ -6,14 +9,26 @@ from fbapp.views import app
 db = SQLAlchemy(app)
 
 
+class Gender (enum.Enum):
+    female = 0
+    male = 1
+    other = 2
+
+
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Integer(), nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
 
     def __init__(self, description, gender):
         self.description = description
         self.gender = gender
 
 
-db.create_all()
+def init_db():
+    db.drop_all()
+    db.create_all()
+    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", Gender['male']))
+    db.session.add(Content("What's your favorite scary movie?", Gender['female']))
+    db.session.commit()
+    lg.warning('Database initialized!')
